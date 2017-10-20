4dab26ccaf2d76126210331d55a6fb8ca1d19c75
elsajmayer <elsajmayer@gmail.com>
2017-10-19 18:43:39 -0700

    first commit

---
+++ 02.js
@@ -0,0 +1,9 @@
+$('li ul li').addClass('special');
+$('table td:nth-child(3)').addClass('year');
+$('table tr:nth(3)').addClass('special');
+$('li a').nextSibling(function(){
+  $('li').addClass('afterlink')
+});
+$('href').contains('pdflink', function(){
+  $('li').closestAncestor('tragedy')
+});
